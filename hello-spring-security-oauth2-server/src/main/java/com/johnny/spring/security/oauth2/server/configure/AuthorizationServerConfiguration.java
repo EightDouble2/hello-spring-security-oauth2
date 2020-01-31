aa9a32e923e7c8e059d56905932aa6c5af1e3bb9
吴昊 <johnny173862903@gmail.com>
2020-01-31 22:04:50 +0800

    基于JDBC存储令牌

--- hello-spring-security-oauth2-server/src/main/java/com/johnny/spring/security/oauth2/server/configure/AuthorizationServerConfiguration.java
+++ hello-spring-security-oauth2-server/src/main/java/com/johnny/spring/security/oauth2/server/configure/AuthorizationServerConfiguration.java
@@ -1,11 +1,22 @@
 package com.johnny.spring.security.oauth2.server.configure;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
@@ -14,21 +25,47 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
 
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        // 配置数据源
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        // Token放入数据库
+        return new JdbcTokenStore(dataSource());
+    }
+
+    @Bean
+    public ClientDetailsService jdbcClientDetailsService() {
+        // 从数据库读取客户端配置
+        return new JdbcClientDetailsService(dataSource());
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore());
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        // 配置客户端
-        clients
-                // 使用内存设置
-                .inMemory()
-                // client_id
-                .withClient("client")
-                // client_secret
-                .secret(passwordEncoder.encode("secret"))
-                // 授权类型
-                .authorizedGrantTypes("authorization_code")
-                // 授权范围
-                .scopes("app")
-                // 注册回调地址
-                .redirectUris("https://www.baidu.com");
+//        // 配置客户端
+//        clients
+//                // 使用内存设置
+//                .inMemory()
+//                // client_id
+//                .withClient("client")
+//                // client_secret
+//                .secret(passwordEncoder.encode("secret"))
+//                // 授权类型
+//                .authorizedGrantTypes("authorization_code")
+//                // 授权范围
+//                .scopes("app")
+//                // 注册回调地址
+//                .redirectUris("https://www.baidu.com");
+        clients.withClientDetails(jdbcClientDetailsService());
     }
 }
